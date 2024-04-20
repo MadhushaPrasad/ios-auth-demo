74fc5ceb25a2420c78e0714328267e37e9dbb880
MadhushaPrasad <madushaprasad21@gmail.com>
2024-04-20 10:58:56 +0530

    Initial Commit

---
+++ ios-auth-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-auth-demo
+//
+//  Created by Madhusha Prasad on 2024-04-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
